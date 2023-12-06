4aa92b696f2f20af9a375c98a5f4d32f59994da9
Vicky-Kaseger <151595334+Vicky-Kaseger@users.noreply.github.com>
2023-12-06 19:56:11 +0800

    Update hello_world.cpp  - Added names

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,11 +2,15 @@
 using namespace std;
 
 void balasan(){
-    cout << "\nHalo manusia !";
+    cout << "\nHalo manusia !" << endl;
 }
 
 int main(){
+    string nama;
     cout << "Hallo dunia !";
     balasan();
+    cout << "Namamu siapa?" << endl;
+    getline(cin, nama);
+    cout << endl << "Hallo " << nama << "!";
     return 0;
 }
